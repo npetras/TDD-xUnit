7f784bf5630b4627dcd23417d3f40bffa2664950
Nicolas <nicolaspetras@gmail.com>
2020-05-10 17:19:46 +0100

    Some Updates from Chapter 19

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -3,8 +3,12 @@ from WasRun import WasRun
 
 
 class TestCaseTest(TestCase):
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert test.wasSetup
+
     def testRunning(self):
         test = WasRun("testMethod")
-        assert not test.wasRun
         test.run()
         assert test.wasRun
--- WasRun.py
+++ WasRun.py
@@ -3,8 +3,13 @@ from TestCase import TestCase
 
 class WasRun(TestCase):
     def __init__(self, name):
+        self.wasSetup = None
         self.wasRun = None
         TestCase.__init__(self, name)
+
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetup = 1
 
     def run(self):
         method = getattr(self, self.name)
