2bdd63437f14b8dcfeccfdce54f39643d5da1e2d
Nicolas <nicolaspetras@gmail.com>
2020-05-11 13:23:30 +0100

    Chapter 19 and 20 updates

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -3,12 +3,13 @@ from WasRun import WasRun
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+    
     def testSetUp(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert test.wasSetup
+        self.test.run()
+        assert "setUasdp " == self.test.log
 
     def testRunning(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert test.wasRun
+        self.test.run()
+        assert self.test.wasRun
--- WasRun.py
+++ WasRun.py
@@ -3,18 +3,17 @@ from TestCase import TestCase
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasSetup = None
-        self.wasRun = None
+        self.log = ""
         TestCase.__init__(self, name)
 
     def setUp(self):
         self.wasRun = None
-        self.wasSetup = 1
+        self.log = "setUp "
 
     def run(self):
         method = getattr(self, self.name)
         method()
 
     def testMethod(self):
-        self.wasRun = 1
+        self.log = self.log + "testMethod "
         pass
